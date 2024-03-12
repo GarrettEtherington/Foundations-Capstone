1f2e2b78f8fe48034feab34a086b9dfdd259c5fd
Garrett Etherington <pogglesnash@gmail.com>
2024-03-11 20:24:04 -0600

    base functionallity

--- public/lamp1.js
+++ public/lamp1.js
@@ -1,54 +1,87 @@
-const db = require('../db.json')
-const list = require('../list.json')
-
 const travelL = document.getElementById("currentL")
 const babeContainer = document.getElementById("babesList")
+const button1 = document.getElementById("butt1")
+const button2 = document.getElementById("butt2")
+const babs = document.getElementById("babeImage")
+const wordZ = document.getElementById("writing")
 
-const baseURL = `/api/babes`
-const babeCallback = ({ data: babes }) => showBabes(babes)
+const baseURL = "http://localhost:4545/api/babes"
 const errCallback = err => console.log(err);
 
 const moveTo = (endpoint) => {
     window.location.pathname = `public/${endpoint}.html`
-};
-
-const getAllBabes = () => {
-    console.log(`something`)
-    axios.get(baseURL).then(babeCallback).catch(errCallback)
 }
 
-const findBabe = (id) => {
-    const index = db.findIndex(babe => {
-        return babe.id === +id
-    })
-
-    return db[index]
+const getAllBabes = () => {
+    axios.get(baseURL).then(showBabes).catch(errCallback)
 }
 
 const addBabe = (id) => {
-    const babe = findBabe(id)
-    axios.post(baseURL, babe).then(babeCallback).catch(errCallback)
+    axios.get(`http://localhost:4545/api/findbabe/${id}`).then(response => {
+        const babe = response.data
+        axios.post(baseURL, babe).then(getAllBabes).catch(errCallback)
+    }).catch(errCallback)
 }
 
 const deleteBabe = (id) => {
-    axios.delete(`${baseURL}/${id}`).then(babeCallback).catch(errCallback)
+    axios.delete(`${baseURL}/${id}`).then(getAllBabes).catch(errCallback)
 }
 
-const updateBabe = (id) => {
-    const name = {name: input.value}
-    axios.put(`${baseURL}/${id}`, name).then(babeCallback).catch(errCallback)
+const updateBabe = (id, name) => {
+    const newName = {name: name}
+    console.log("new", newName)
+    axios.put(`${baseURL}/${id}`, newName).then(getAllBabes).catch(errCallback)
 }
 
-
-
-function showBabes(arr) {
-    arr.forEach(babe => {
-        const div = document.createElement('li')
-        div.textContent = babe.name
-        babeContainer.appendChild(div)
+const gimmieBabe = (id) => {
+    axios.get(`http://localhost:4545/api/findbabe/${id}`).then(response => {
+        const foundBabe = response.data
+        babs.src = foundBabe.img
+        babs.setAttribute('data-custom', id);
+        wordZ.innerText = foundBabe.word
     })
 }
 
-travelL.addEventListener("change", () => {moveTo(travelL.value)})
 
-getAllBabes()
+
+function showBabes({ data: arr }) {
+    babeContainer.innerHTML = '';
+    arr.forEach(babe => {
+        const babeHolder = document.createElement("li");
+        babeHolder.style.display = "flex";
+        babeContainer.style.padding = "0";
+        babeHolder.style.justifyContent = "space-between";
+
+        const babeName = document.createElement("p");
+        const deleteBtn = document.createElement("button");
+        deleteBtn.style.margin = "15px 0";
+
+        babeName.textContent = babe.name;
+        deleteBtn.textContent = "x";
+
+        deleteBtn.addEventListener("click", deleteBabe);
+
+        babeName.addEventListener("click", () => {
+            const input = document.createElement("input");
+            input.type = "text";
+            input.value = babeName.textContent;
+            input.addEventListener("blur", () => {
+                updateBabe(babe.id, input.value); 
+                babeHolder.removeChild(input);
+            });
+            babeHolder.insertBefore(input, deleteBtn);
+            babeHolder.removeChild(babeName); 
+            input.focus();
+        });
+        
+
+        babeHolder.appendChild(babeName);
+        babeHolder.appendChild(deleteBtn);
+        babeContainer.appendChild(babeHolder);
+    });
+}
+
+travelL.addEventListener("change", () => {moveTo(travelL.value)})
+button1.addEventListener("click", () => { const buttonValue = button1.value; gimmieBabe(buttonValue)})
+button2.addEventListener("click", () => { const buttonValue = button2.value; gimmieBabe(buttonValue)})
+babs.addEventListener("click", () => {const babeId = babs.getAttribute('data-custom'); addBabe(babeId); getAllBabes})
--- server/controller.js
+++ server/controller.js
@@ -9,26 +9,72 @@ module.exports = {
     //     return db[place]
     // },
   
+    findBabe: (req, res) => {
+        const id = req.params.id
+
+        const index = db.findIndex(babe => {
+            const key = Object.keys(babe)[0];
+            const babeid = babe[key].id
+            return babeid === parseInt(id, 10)
+        })
+
+        const dude = db[index]
+        const key = Object.keys(dude)[0];
+        res.status(200).send(dude[key])
+    },
+
     getBabes: (req, res) => {
-        console.log(`controller babas`)
         res.status(200).send(list)
     },
 
     addBabes: (req, res) => {
-        const babe = req.body.babe
-        list.push(babe)
-        res.status(200)
+        const babe = req.body
+        const alreadyExists = list.some(existingBabe => existingBabe.id === babe.id);
+        if (!alreadyExists) {
+            list.push(babe);
+            res.status(200).send('Babe added successfully');
+        } else {
+            console.log('Babe already exists in the list');
+            res.status(400).send('Babe already exists in the list');
+        }
     },
 
     removeBabe: (req, res) => {
         const id = req.params.id
 
         const index = list.findIndex(babe => {
-            return babe.id === +id
+            return babe.id === parseInt(id, 10)
         })
         list.splice(index, 1)
         res.status(200).send(list)
-    }
+    },
 
+    updateBabe: (req, res) => {
+        const id = req.params.id;
+        const { name } = req.body;
+    
+        const index = list.findIndex(babe => {
+            const babeid = babe.id;
+            return babeid === parseInt(id, 10);
+        });
+
+        
+        if (index === -1) {
+            return res.status(404).send('Babe not found');
+        }
+        
+        const babe = list[index];
+        babe.name = name;
+    
+        res.status(200).send(babe);
+    },
+    
+    becomeChad: (req, res) => {
+        if(list.length === 7) {
+            // Move to chad page
+        } else {
+            // alert nbot enouigh moths
+        }
+    }
 }
 
